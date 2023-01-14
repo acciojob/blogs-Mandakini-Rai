416febe7b452cddf5a03298a1077c862e1ab31b6
Ashish Rai <mandakinirai2410@gmail.com>
2023-01-14 19:13:49 +0530

    third

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -62,21 +62,14 @@ public class BlogService {
 
     public void addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog after creating it
-        Image image = new Image();
-        image.setDimensions(dimensions);
-        image.setDescription(description);
-
-        Blog blog = blogRepository1.findById(blogId).get();
+        Blog blog=blogRepository1.findById(blogId).get();
+        Image image=imageService1.createAndReturn(blog,description,dimensions);
         image.setBlog(blog);
-
-        List<Image> imageList = blog.getImageList();
+        List<Image> imageList=blog.getImageList();
         if(imageList==null) imageList=new ArrayList<>();
         imageList.add(image);
-
         blog.setImageList(imageList);
-
         blogRepository1.save(blog);
-
 
     }
 
